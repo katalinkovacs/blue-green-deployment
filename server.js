4f023ff39ce23cf5930bb790b2d88a7eb0a72f4a
katalinkovacs <katalinkovacsnz@gmail.com>
2018-07-25 11:13:06 +1000

    Some chnges, added ejs and body-parser --> working ok

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+var RUN_PORT = 8080;
+
+
+app.set('views', __dirname + '/views');
+app.engine('json', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-   res.send('<h1>Welcome!</h1>');
 
+//some.html routing
+app.get('/', function(req, res){
+    res.render("green.html");
 });
 
 
-// /bad - send back json with errorMessage
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
+//error.html routing
+app.get('/error', function(req, res){
+    res.render("error.html");
+});
+
+//showing that the program is running on the RUN_PORT
+app.listen(RUN_PORT, function(){
+    console.log("Running on port " + RUN_PORT);
 });
 
 
-app.listen(8080, () => {
-  console.log('Server is up on port 3000');
-});
+
+
